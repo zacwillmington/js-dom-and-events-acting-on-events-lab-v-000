bafd519258122bd5ed0f405ba0e56ad6744a5191
Zac Willmington <zac.willmington@gmail.com>
2018-09-19 18:10:11 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,31 @@ function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
     })
+
 }
 
 preventRefreshOnSubmit()
+
+
+function retrieveEmployeeInformation() {
+    return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+    const li = document.createElement('li')
+     li.innerHTML = retrieveEmployeeInformation();
+    return $('ul').first().append(li);
+}
+
+function addNewLiOnClick() {
+    $('form').on('click', function() {
+        addNewElementAsLi();
+        $('input[type=text]').val('');
+    });
+}
+
+function clearEmployeeListOnLinkClick() {
+    $('.clear-employee-list').on('click', function(){
+        $('ul.employee-list').empty();
+    });
+}
